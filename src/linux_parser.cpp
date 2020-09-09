19f387e4a0a462db2b76cae47dac4482b672013f
Bidilă Timotei <31857468+19Timotei97@users.noreply.github.com>
2020-09-09 22:27:49 +0300

    Added attempt at using std::filesystem

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -93,6 +93,47 @@ std::vector<int> LinuxParser::Pids() {
   return pids;
 }
 
+/*
+An attempt at using std::filesystem
+
+// Updated with std::filesystem
+vector<int> LinuxParser::Pids() {
+  vector<int> pids;
+  
+//   DIR* directory = opendir(kProcDirectory.c_str());
+  
+  const fs::path directory = kProcDirectory.c_str();
+  
+//   struct dirent* file;
+  
+//   while ((file = readdir(directory)) != nullptr) {
+    for (auto& file : fs::directory_iterator(directory)) 
+    {
+    // Is this a directory?
+//     if (file->d_type == DT_DIR) {
+    
+    // Is this a directory?
+    if (fs::is_directory(file.path())) {
+      
+      // Is every character of the name a digit?
+      string filename(file.path().stem().string());
+      
+      if (std::all_of(filename.begin(), filename.end(), isdigit)) {
+        
+        int pid = stoi(filename);
+        pids.push_back(pid);
+      }
+    }
+  }
+  
+//   close(directory);
+  
+  return pids;
+}
+
+*/
+
+
 // Read and return the system memory utilization
 float LinuxParser::MemoryUtilization() 
 {
